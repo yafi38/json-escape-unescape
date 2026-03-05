477f88b07b2da17ccb81caf0c08081e17812941c
kayanika <anikamonirmim@gmail.com>
2026-03-05 23:57:00 +0600

    add fallback to handle invalid JSON string

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -118,4 +118,62 @@ suite('JSON Escape / Unescape Extension', () => {
 			`SELECT *\nFROM "TABLE_A"\nWHERE COLUMN_A = 'ABC';`,
 		);
 	});
+
+	test('unescape command unescapes string with trailing quotes', async () => {
+		const document = await vscode.workspace.openTextDocument({
+			language: 'plaintext',
+			content: 'SELECT *\\nFROM \\"TABLE_A\\"',
+		});
+
+		const editor = await vscode.window.showTextDocument(document);
+		editor.selection = new vscode.Selection(0, 0, 0, 0);
+
+		await vscode.commands.executeCommand('json-escape-unescape.unescape');
+
+		const updatedText = document.getText();
+
+		assert.strictEqual(
+			updatedText,
+			`SELECT *\nFROM "TABLE_A"`,
+		);
+	});
+
+	test('unescape command unescapes string with missing braces', async () => {
+		const document = await vscode.workspace.openTextDocument({
+			language: 'plaintext',
+			content: '"file_path": \"C:\\\\Program Files\\\\App\"',
+		});
+
+		const editor = await vscode.window.showTextDocument(document);
+		editor.selection = new vscode.Selection(0, 0, 0, 0);
+
+		await vscode.commands.executeCommand('json-escape-unescape.unescape');
+
+		const updatedText = document.getText();
+
+		assert.strictEqual(
+			updatedText,
+			`"file_path": "C:\\Program Files\\App"`,
+		);
+	});
+
+	test('unescape command unescapes invalid JSON with unicode characters', async () => {
+		const document = await vscode.workspace.openTextDocument({
+			language: 'plaintext',
+			content: '[ \"Item 1\", \"Item 2\\u00A9\", ]',
+		});
+
+		const editor = await vscode.window.showTextDocument(document);
+		editor.selection = new vscode.Selection(0, 0, 0, 0);
+
+		await vscode.commands.executeCommand('json-escape-unescape.unescape');
+
+		const updatedText = document.getText();
+
+		assert.strictEqual(
+			updatedText,
+			`[ "Item 1", "Item 2©", ]`,
+		);
+	});
+
 });
