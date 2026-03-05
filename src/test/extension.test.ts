6b9c5cbad9da1f35cff24e48fea9e8792ec3646b
Mashrur Ahmed Yafi <34958229+yafi38@users.noreply.github.com>
2026-03-05 13:57:52 +0600

    add basic unescape test (#2)

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -54,4 +54,29 @@ suite('JSON Escape / Unescape Extension', () => {
 			`SELECT \\"COLUMN_A\\"\nFROM \\"TABLE B\\"`,
 		);
 	});
+
+	test('unescape command unescapes characters in selected text', async () => {
+		const document = await vscode.workspace.openTextDocument({
+			language: 'plaintext',
+			content: 'SELECT *\\nFROM \\"TABLE_A\\"\\nWHERE COLUMN_A = \'ABC\';'
+		});
+
+		const editor = await vscode.window.showTextDocument(document);
+
+		const firstLine = document.lineAt(0);
+		const lastLine = document.lineAt(document.lineCount - 1);
+		editor.selection = new vscode.Selection(
+			firstLine.range.start,
+			lastLine.range.end,
+		);
+
+		await vscode.commands.executeCommand('json-escape-unescape.unescape');
+
+		const updatedText = document.getText();
+
+		assert.strictEqual(
+			updatedText,
+			`SELECT *\nFROM "TABLE_A"\nWHERE COLUMN_A = 'ABC';`
+		);
+	});
 });
