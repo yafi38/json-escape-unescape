4c77fe19a93473eebd0801dec1bbc45b16238010
yafi38 <yafi2475@gmail.com>
2026-03-06 23:47:23 +0600

    move process text to a separate file

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 import { escape } from './escape';
 import { unescape } from './unescape';
+import { processText } from './processText';
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -13,43 +14,6 @@ export function activate(context: vscode.ExtensionContext) {
     });
 
     context.subscriptions.push(escapeCmd, unescapeCmd);
-}
-
-function processText(transform: (text: string) => string): Thenable<boolean> | void {
-    const editor = vscode.window.activeTextEditor;
-
-    if (!editor) {
-        return;
-    }
-
-    return editor.edit(editBuilder => {
-        const selections = getSelections(editor);
-
-        selections.forEach(selection => {
-            const text = editor.document.getText(selection);
-
-            if (text) {
-                editBuilder.replace(selection, transform(text));
-            }
-        });
-    });
-}
-
-function getSelections(editor: vscode.TextEditor): vscode.Selection[] {
-    const nonEmptySelections = editor.selections.filter(s => !s.isEmpty);
-
-    if (nonEmptySelections.length > 0) {
-        return nonEmptySelections;
-    }
-
-    const fullDocumentSelection = new vscode.Selection(
-        0,
-        0,
-        editor.document.lineCount - 1,
-        editor.document.lineAt(editor.document.lineCount - 1).range.end.character,
-    );
-
-    return [fullDocumentSelection];
 }
 
 export function deactivate() { }
