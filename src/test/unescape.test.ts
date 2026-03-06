1007672f2d6dc5030a012b6180cb70044420d1db
yafi38 <yafi2475@gmail.com>
2026-03-06 23:37:40 +0600

    move escape and unescape tests to different files

--- src/test/unescape.test.ts
+++ src/test/unescape.test.ts
@@ -1,80 +1,7 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
 
-suite('JSON Escape / Unescape Extension', () => {
-	test('escape command escapes quotes and newlines in selected text', async () => {
-		const document = await vscode.workspace.openTextDocument({
-			language: 'plaintext',
-			content: 'SELECT *\nFROM "TABLE_A"\nWHERE COLUMN_A = \'ABC\';',
-		});
-
-		const editor = await vscode.window.showTextDocument(document);
-
-		const firstLine = document.lineAt(0);
-		const lastLine = document.lineAt(document.lineCount - 1);
-		editor.selection = new vscode.Selection(
-			firstLine.range.start,
-			lastLine.range.end,
-		);
-
-		await vscode.commands.executeCommand('json-escape-unescape.escape');
-
-		const updatedText = document.getText();
-		assert.strictEqual(
-			updatedText,
-			`SELECT *\\nFROM \\"TABLE_A\\"\\nWHERE COLUMN_A = 'ABC';`,
-		);
-	});
-
-	test('escape command escapes whole document when there is no selection', async () => {
-		const document = await vscode.workspace.openTextDocument({
-			language: 'plaintext',
-			content: 'SELECT *\nFROM "TABLE_A"\nWHERE COLUMN_A = \'ABC\';',
-		});
-
-		const editor = await vscode.window.showTextDocument(document);
-		editor.selection = new vscode.Selection(0, 0, 0, 0);
-
-		await vscode.commands.executeCommand('json-escape-unescape.escape');
-
-		const updatedText = document.getText();
-
-		assert.strictEqual(
-			updatedText,
-			`SELECT *\\nFROM \\"TABLE_A\\"\\nWHERE COLUMN_A = 'ABC';`,
-		);
-	});
-
-	test('escape command handles multiple selections independently', async () => {
-		const document = await vscode.workspace.openTextDocument({
-			language: 'plaintext',
-			content: 'SELECT "COLUMN_A"\nFROM "TABLE B"',
-		});
-
-		const editor = await vscode.window.showTextDocument(document);
-
-		const firstLineText = document.lineAt(0).text;
-		const firstStartCol = firstLineText.indexOf('"COLUMN_A"');
-		const firstEndCol = firstStartCol + '"COLUMN_A"'.length;
-
-		const secondLineText = document.lineAt(1).text;
-		const secondStartCol = secondLineText.indexOf('"TABLE B"');
-		const secondEndCol = secondStartCol + '"TABLE B"'.length;
-
-		const firstSelection = new vscode.Selection(0, firstStartCol, 0, firstEndCol);
-		const secondSelection = new vscode.Selection(1, secondStartCol, 1, secondEndCol);
-		editor.selections = [firstSelection, secondSelection];
-
-		await vscode.commands.executeCommand('json-escape-unescape.escape');
-
-		const updatedText = document.getText();
-
-		assert.strictEqual(
-			updatedText,
-			`SELECT \\"COLUMN_A\\"\nFROM \\"TABLE B\\"`,
-		);
-	});
-
+suite('JSON Unescape Tests', () => {
 	test('unescape command unescapes characters in selected text', async () => {
 		const document = await vscode.workspace.openTextDocument({
 			language: 'plaintext',
@@ -175,5 +102,4 @@ suite('JSON Escape / Unescape Extension', () => {
 			`[ "Item 1", "Item 2©", ]`,
 		);
 	});
-
 });
